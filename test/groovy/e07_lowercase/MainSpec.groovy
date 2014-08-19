5648f7761399508c17f875e763a6c50885641f05
Zarsova <shrsakai@gmail.com>
2014-08-19 20:38:10 +0900

    java/src/e08_sum_of_digits/

---
+++ test/groovy/e07_lowercase/MainSpec.groovy
@@ -0,0 +1,24 @@
+package e07_lowercase
+
+import spock.lang.Specification
+
+class MainSpec extends Specification {
+    def "should return correct test outputs given test input"() {
+        given:
+        def savedSystemOut = System.out
+        def ps = Mock(PrintStream)
+        System.out = ps
+
+        when:
+        Main.main(["input/e07-lowercase.txt"] as String[])
+
+        then:
+        1 * ps.println("hello codeeval")
+
+        then:
+        1 * ps.println("this is some text")
+
+        cleanup:
+        System.out = savedSystemOut
+    }
+}
