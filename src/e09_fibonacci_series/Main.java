96d8236a7d43c83fa1eab4641f5b55bdbeb95ad1
Zarsova <shrsakai@gmail.com>
2014-08-19 22:09:25 +0900

    java/src/e10_multiplication_tables/

--- src/e09_fibonacci_series/Main.java
+++ src/e09_fibonacci_series/Main.java
@@ -7,27 +7,26 @@ import java.io.IOException;
 import java.util.HashMap;
 
 public class Main {
-    private static final Main instance = new Main();
 
-    public static Main getInstance() {
-        return instance;
-    }
+    private static final HashMap<Integer, Integer> fibs = new HashMap<>();
 
-    private final HashMap<Integer, Integer> fibs = new HashMap<>();
-
-    public int calc(int n) {
+    public static int calc(int n) {
         if (n <= 1) return n;
         if (n == 2) return 1;
-        if (fibs.containsKey(n)) return fibs.get(n);
-        fibs.put(n, calc(n - 1) + calc(n - 2));
-        return fibs.get(n);
+        if (fibs.containsKey(n)) {
+            return fibs.get(n);
+        } else {
+            int ret = calc(n - 1) + calc(n - 2);
+            fibs.put(n, ret);
+            return ret;
+        }
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new FileReader(new File(args[0])));
         String line;
         while ((line = in.readLine()) != null) {
-            int fib = getInstance().calc(Integer.parseInt(line));
+            int fib = calc(Integer.parseInt(line));
             System.out.println(Integer.toString(fib));
         }
     }
--- test/groovy/e09_fibonacci_series/MainSpec.groovy
+++ test/groovy/e09_fibonacci_series/MainSpec.groovy
@@ -21,4 +21,27 @@ class MainSpec extends Specification {
         cleanup:
         System.out = savedSystemOut
     }
+
+    def "check peformance"() {
+        given:
+        def loop =  100000
+        def printErr = System.err.&println
+        def savedSystemOut = System.out
+        def ps = Mock(PrintStream)
+        System.out = ps
+
+        when:
+        def start = System.currentTimeMillis()
+        (1..100000).each {
+            Main.main(["input/e09_fibonacci_series.txt"] as String[])
+        }
+        def duration = System.currentTimeMillis() - start
+        printErr("${loop} time: ${duration} msec (${duration * 1.0 / loop} msec)")
+
+        then:
+        true
+
+        cleanup:
+        System.out = savedSystemOut
+    }
 }
