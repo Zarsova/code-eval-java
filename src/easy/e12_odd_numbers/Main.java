217dbe1f58e8598e4be3bfa88b31b33ee91aab53
Zarsova <shrsakai@gmail.com>
2014-08-21 17:20:37 +0900

    src/easy/e12_odd_numbers/

---
+++ src/easy/e12_odd_numbers/Main.java
@@ -0,0 +1,9 @@
+package easy.e12_odd_numbers;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i < 100; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
