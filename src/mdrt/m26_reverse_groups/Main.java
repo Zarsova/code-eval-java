03ebeaffeba1925fd9fcff646c82291746834bd9
Zarsova <shrsakai@gmail.com>
2014-09-17 18:14:45 +0900

    java/src/mdrt/m26_reverse_groups/Main.java

--- src/mdrt/m26_reverse_groups/Main.java
+++ src/mdrt/m26_reverse_groups/Main.java
@@ -3,7 +3,6 @@ package mdrt.m26_reverse_groups;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
 
 public class Main implements Runnable {
     private final String fileName;
